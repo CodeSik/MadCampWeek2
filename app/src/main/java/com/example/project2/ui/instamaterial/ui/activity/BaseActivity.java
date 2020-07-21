89aaa1cc4a24dffcccbb225d045f97724f1497ab
GeonsikSeo <iqeq2328@gmail.com>
2020-07-21 16:26:17 +0900

    BaseActivity

---
+++ app/src/main/java/com/example/project2/ui/instamaterial/ui/activity/BaseActivity.java
@@ -0,0 +1,76 @@
+package com.example.project2.ui.instamaterial.ui.activity;
+
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+import com.example.project2.R;
+
+
+/**
+ * Created by Miroslaw Stanek on 19.01.15.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Nullable
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Nullable
+    @BindView(R.id.ivLogo)
+    ImageView ivLogo;
+
+    private MenuItem inboxMenuItem;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        bindViews();
+    }
+
+    protected void bindViews() {
+        ButterKnife.bind(this);
+//        setupToolbar();
+    }
+
+    public void setContentViewWithoutInject(int layoutResId) {
+        super.setContentView(layoutResId);
+    }
+
+    protected void setupToolbar() {
+        if (toolbar != null) {
+//            setSupportActionBar(toolbar);
+           // toolbar.setNavigationIcon(R.drawable.);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        inboxMenuItem = menu.findItem(R.id.action_inbox);
+        inboxMenuItem.setActionView(R.layout.menu_item_view);
+        return true;
+    }
+
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+
+    public MenuItem getInboxMenuItem() {
+        return inboxMenuItem;
+    }
+
+    public ImageView getIvLogo() {
+        return ivLogo;
+    }
+}
